348bb5be05721da97b7e0575eb5e83f584fb9242
Federico Churca <federico.churca@gmail.com>
2010-05-18 03:08:11 -0300

    Fixed namespaces bug

--- list.cpp
+++ list.cpp
@@ -5,48 +5,50 @@
 
 #include <stdexcept>
 
-using namespace custom;
+namespace custom{
 
-template <class T> list<T>::list(){
-	this->first = this->last = NULL;
-	this->Size = 0;
-}
-template <class T> list<T>::~List(){
-	this->clear();
-};
-
-template <class T> size_t list<T>::size() const{
-	return this->Size;
-};
-
-template <class T> void list<T>::push_back(const T & data){
-	node * to_add = new node(data, NULL);
-	if (this->last){
-		this->last->next = to_add;
-	}else{
-		this->first = to_add;
+	template <class T> list<T>::list(){
+		this->first = this->last = NULL;
+		this->Size = 0;
 	}
-	this->last = to_add;
-	this->Size++;
-};
+	template <class T> list<T>::~List(){
+		this->clear();
+	};
 
-template <class T> T list<T>::pop_front(){
-	if (this->first){
-		T ret = this->first->data;
-		node * aux = this->first;
-		this->first = this->first->next;
-		delete aux;
-		this->Size--;
-		return ret;
-	}else{
-		throw std::runtime_error("Nothing to pop");
-	}
-};
+	template <class T> size_t list<T>::size() const{
+		return this->Size;
+	};
 
-template <class T> void list<T>::clear(){
-	while(this->first){
-		this->pop_front();
+	template <class T> void list<T>::push_back(const T & data){
+		node * to_add = new node(data, NULL);
+		if (this->last){
+			this->last->next = to_add;
+		}else{
+			this->first = to_add;
+		}
+		this->last = to_add;
+		this->Size++;
+	};
+
+	template <class T> T list<T>::pop_front(){
+		if (this->first){
+			T ret = this->first->data;
+			node * aux = this->first;
+			this->first = this->first->next;
+			delete aux;
+			this->Size--;
+			return ret;
+		}else{
+			throw std::runtime_error("Nothing to pop");
+		}
+	};
+
+	template <class T> void list<T>::clear(){
+		while(this->first){
+			this->pop_front();
+		}
 	}
+
 }
 
 #ifdef LIST_DEBUG
--- vector.cpp
+++ vector.cpp
@@ -5,97 +5,98 @@
 
 #include <stdexcept>
 
-using namespace custom;
+namespace custom{
 
-template <class T> vector<T>::vector(){
-			this->capacity = 1;
-			this->contents = new T[this->capacity];
-			this->Size = 0;
-}
-template <class T> vector<T>::~vector(){
-	delete[] this->contents;
-}
-template <class T> T & vector<T>::at(size_t pos){
-	if (pos < Size){
-		return this->contents[pos];
-	}else{
-		throw std::runtime_error("Outside bonds");
+	template <class T> vector<T>::vector(){
+				this->capacity = 1;
+				this->contents = new T[this->capacity];
+				this->Size = 0;
 	}
-}
-template <class T> size_t vector<T>::size() const{
-	return this->Size;
-}
-template <class T> void vector<T>::clear(){
-	this->Size = 0;
-}
-template <class T> void vector<T>::push_back(const T & data){
-	if (this ->Size == this->capacity){
-		this->change_capacity(this->capacity * 2);
-	}
-	this->contents[this->Size] = data;
-	this->Size++;
-}
-template <class T> T vector<T>::pop_back(){
-	if (this->Size){
-		return this->contents[--(this->Size)];
-	}else{
-		throw std::runtime_error("Nothing to pop");
-	}
-}
-template <class T>
-	void vector<T>::insert(const vector<T>::iterator & it, const T & data){
-	if (it.pos > this->Size){
-		throw std::runtime_error("Outside bonds");
-	}else{
-		this->push_back(data);
-		for(size_t i = this->Size - 1; i > it.pos; i--){
-			T aux = this->at(i);
-			this->at(i) = this->at(i-1);
-			this->at(i-1) = aux;
-		}
-	}
-}
-template <class T> void vector<T>::change_capacity(size_t capacity){
-	if (this->capacity > capacity){
-		throw std::runtime_error("New capacity smaller than old");
-	}else{
-		T * contents = new T [capacity];
-		for(size_t i = 0; i < this->Size; i++){
-			contents[i] = this->contents[i];
-		}
+	template <class T> vector<T>::~vector(){
 		delete[] this->contents;
-		this->contents = contents;
-		this->capacity = capacity;
 	}
-}
-
-template <class T> vector<T>::iterator::iterator(){
-	this->parent = NULL;
-	this->initialized = false;
-}
-template <class T> vector<T>::iterator::iterator(const iterator & newit){
-	this->parent = newit.parent;
-	this->pos = newit.pos;
-	this->at_end = newit.pos;
-	this->initialized = newit.initialized;
-}
-template <class T> T & vector<T>::iterator::operator*(){
-	if (this->initialized){
-		return parent->at(this->pos);
-	}else{
-		throw std::runtime_error("Iterator not initialized");
-	}
-}
-template <class T> void vector<T>::iterator::operator++(){
-	if (this->initialized){
-		if (this->at_end){
-			throw std::runtime_error("Iterator at end");
+	template <class T> T & vector<T>::at(size_t pos){
+		if (pos < Size){
+			return this->contents[pos];
 		}else{
-			this->pos++;
-			this->at_end = (this->pos == this->parent->size());
+			throw std::runtime_error("Outside bonds");
 		}
-	}else{
-		throw std::runtime_error("Iterator not initialized");
+	}
+	template <class T> size_t vector<T>::size() const{
+		return this->Size;
+	}
+	template <class T> void vector<T>::clear(){
+		this->Size = 0;
+	}
+	template <class T> void vector<T>::push_back(const T & data){
+		if (this ->Size == this->capacity){
+			this->change_capacity(this->capacity * 2);
+		}
+		this->contents[this->Size] = data;
+		this->Size++;
+	}
+	template <class T> T vector<T>::pop_back(){
+		if (this->Size){
+			return this->contents[--(this->Size)];
+		}else{
+			throw std::runtime_error("Nothing to pop");
+		}
+	}
+	template <class T>
+		void vector<T>::insert(const vector<T>::iterator & it, const T & data){
+		if (it.pos > this->Size){
+			throw std::runtime_error("Outside bonds");
+		}else{
+			this->push_back(data);
+			for(size_t i = this->Size - 1; i > it.pos; i--){
+				T aux = this->at(i);
+				this->at(i) = this->at(i-1);
+				this->at(i-1) = aux;
+			}
+		}
+	}
+	template <class T> void vector<T>::change_capacity(size_t capacity){
+		if (this->capacity > capacity){
+			throw std::runtime_error("New capacity smaller than old");
+		}else{
+			T * contents = new T [capacity];
+			for(size_t i = 0; i < this->Size; i++){
+				contents[i] = this->contents[i];
+			}
+			delete[] this->contents;
+			this->contents = contents;
+			this->capacity = capacity;
+		}
+	}
+
+	template <class T> vector<T>::iterator::iterator(){
+		this->parent = NULL;
+		this->initialized = false;
+	}
+	template <class T> vector<T>::iterator::iterator(const iterator & newit){
+		this->parent = newit.parent;
+		this->pos = newit.pos;
+		this->at_end = newit.pos;
+		this->initialized = newit.initialized;
+	}
+	template <class T> T & vector<T>::iterator::operator*(){
+		if (this->initialized){
+			return parent->at(this->pos);
+		}else{
+			throw std::runtime_error("Iterator not initialized");
+		}
+	}
+	template <class T> void vector<T>::iterator::operator++(){
+		if (this->initialized){
+			if (this->at_end){
+				throw std::runtime_error("Iterator at end");
+			}else{
+				this->pos++;
+				this->at_end = (this->pos == this->parent->size());
+			}
+		}else{
+			throw std::runtime_error("Iterator not initialized");
+		}
 	}
 }
 #endif	//	__VECTOR_CPP__
