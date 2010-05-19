0678d23450782f6795f9cb37827f91ce151f589b
Federico Churca <federico@fchurca-ubuntu32.(none)>
2010-05-19 15:10:04 -0300

    Started shell

--- main.cpp
+++ main.cpp
@@ -8,52 +8,84 @@
 
 #include <stdexcept>
 #include <iostream>
+#include <sstream>
+
+void parser(std::istream & in, std::ostream & out, FSModel & model);
 
 using namespace std;
 
 int main(int argc, char **argv){
 	int ret = EXIT_SUCCESS;
-	if (argc > 2){
-		if (strcmp(argv[1], "-d")){
-			cerr << "Expected \"-d\" as first parameter" << endl;
-			ret = EXIT_FAILURE;
-		}else try{
-			FSModel mymodel;
-			Cronometro cron;
-			mymodel.load(argv[2]);
-			cron.parar();
-			mymodel.dump(cout);
-			cout
-				<< "Load finished: " << cron.getTiempoTranscurrido() << " msec" << endl
-				<< "Program terminates \
-with EOF (control-D (*NIX) / control-Z (Windows/DOS))"
-				<< endl;
-			while (cin){
-				string expression;
-				cout << "Enter a search expression:" << endl;
-				getline(cin, expression);
-				if (cin){
-					cron.iniciar();
-					mymodel.search(cout, expression);
-					cron.parar();
-					cout
-						<< "Search completed in "
-						<< cron.getTiempoTranscurrido() << " msec" << endl;
-				}
+	try{
+		FSModel mymodel;
+		if (argc > 2){
+			if (!strcmp(argv[1], "-d")){
+				Cronometro cron;
+				mymodel.load(argv[2]);
+				cron.parar();
+				cout
+					<< "Load finished: " << cron.getTiempoTranscurrido()
+					<< " ms" << endl
+					<< "Loaded " << mymodel.getsize() << " elements" << endl;
 			}
-		}catch(std::runtime_error e){
-			cerr << e.what() << endl << "Aborting" << endl;
-			ret = EXIT_FAILURE;
-		}catch(std::exception e){
-			cerr << e.what() << endl << "Aborting" << endl;
-		}catch(...){
-			cerr << "Unhandled exception" << endl << "Aborting" << endl;
-			ret = EXIT_FAILURE;
 		}
-	}else{
-		cout
-			<< "Usage:\t" << argv[0] << " -d <path>" << endl;
-		;
+		parser(cin, cout, mymodel);
+	}catch(std::runtime_error e){
+		cerr << e.what() << endl << "Aborting" << endl;
+		ret = EXIT_FAILURE;
+	}catch(std::exception e){
+		cerr << e.what() << endl << "Aborting" << endl;
+	}catch(...){
+		cerr << "Unhandled exception" << endl << "Aborting" << endl;
+		ret = EXIT_FAILURE;
 	}
 	return ret;
+}
+
+void parser(std::istream & in, std::ostream & out, FSModel & model){
+	Cronometro cron;
+	string expression;
+	unsigned long
+		minsize = 0,
+		maxsize = -1;
+	while (cin){
+		string dump, command;
+		stringstream ss;
+		out << "> ";
+		getline(cin, dump);
+		ss << dump;
+		ss >> command;
+		if (command == "exit"){
+			return;
+		}else if (command == "dump"){
+			model.dump(out);
+		}else if (command == "expression"){
+			ss >> ws;
+			getline(ss, expression);
+		}else if (command == "load"){
+			ss >> ws;
+			getline(ss, dump);
+			model.clear();
+			model.load(dump);
+		}else if (command == "maxsize"){
+			ss >> dump;
+			maxsize = atol(dump.c_str());
+		}else if (command == "minsize"){
+			ss >> dump;
+			minsize = atol(dump.c_str());
+		}else if (command == "search"){
+			cron.iniciar();
+			model.search(out, expression, maxsize, minsize);
+			cron.parar();
+			out
+				<< "Search completed in "
+				<< cron.getTiempoTranscurrido() << " msec" << endl;
+		}else if (command == "status"){
+			out
+				<< "Path:         " << model.getpath() << endl
+				<< "Expression:   " << expression << '\"' << endl
+				<< "Minimum size: " << minsize << endl
+				<< "Maximum size: " << maxsize << endl;
+		}
+	}	
 }
