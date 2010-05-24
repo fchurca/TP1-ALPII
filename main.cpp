804168e63a8c58a07130a8252d69daeb1d31c26a
Federico Churca <federico.churca@gmail.com>
2010-05-24 01:21:10 -0300

    Settled for plain optionless parameters

--- main.cpp
+++ main.cpp
@@ -3,9 +3,6 @@
 #include "fsmodel.h"
 #include "Cronometro.h"
 
-#include <cstdlib>
-#include <cstring>
-
 #include <stdexcept>
 #include <iostream>
 
@@ -13,37 +10,29 @@ using namespace std;
 
 int main(int argc, char **argv){
 	int ret = EXIT_SUCCESS;
-	FSModel mymodel;
 	try{
-// Stub for console parameter parser
-		for (int i = 1, next = 2; i < argc; i = next){
-			if (*argv[i] == '-'){
-			}else{
-				cerr
-					<< "Parameter invalid: " << argv[i] << endl
-					<< "Did you mean \" -d " << argv[i] <<"\"?" << endl;
-			}
-			next++;
-		}
-		if (argc > 2){
-			if (!strcmp(argv[1], "-d")){
-				Cronometro cron;
-				mymodel.load(argv[2]);
+		FSModel mymodel;
+		Cronometro cron;
+		for (unsigned i = 1; i < argc; i++){
+			try{
+				cron.iniciar();
+				mymodel.load(argv[i]);
 				cron.parar();
 				cout
 					<< "Load finished: " << cron.getTiempoTranscurrido()
 					<< " ms" << endl
-					<< "Loaded " << mymodel.getsize() << " elements" << endl;
+					<< "Loaded " << mymodel.getsize()
+					<< " elements" << endl;			
+			}catch(runtime_error e){
+				cerr << e.what() << endl;
 			}
 		}
 		cout << "Type \"help\"<return> for help" << endl;
 		parser(cin, cout, mymodel);
-	}catch(std::runtime_error e){
-		mymodel.clear();
+	}catch(logic_error e){
 		cerr << e.what() << endl << "Aborting" << endl;
 		ret = EXIT_FAILURE;
 	}catch(...){
-		mymodel.clear();
 		cerr << "Unknown exception. Aborting" << endl;
 		ret = EXIT_FAILURE;
 	}
